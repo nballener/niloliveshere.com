391544ac43671bf4ed607deac8895d2aa30a89f7
Nilo Ballener <nballener@gmail.com>
2015-11-17 01:28:52 +1100

    Setup some of the structure for the website

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,39 @@
 <!doctype html>
 <html>
-  <head>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Nilo Lives Here</title>
         <link rel="stylesheet" href="css/uikit.min.css" />
-        <script src="js/jquery.js"></script>
+        <link rel="stylesheet" href="css/uikit.almost-flat.min.css" />
+        <script src="js/jquery.min.js"></script>
         <script src="js/uikit.min.js"></script>
     </head>
     <body>
-      winning
+        <nav class="uk-navbar uk-block uk-text-center">
+            <ul class="uk-navbar-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#"><i class="uk-icon-cog uk-icon-large"></i></a></li>
+            </ul>
+        </nav>
+        <div class="uk-block uk-block-primary uk-block-large">
+            <div class="uk-container uk-text-center uk-text-contrast">
+                <h1 class="uk-h1 uk-heading-large uk-text-contrast">Nilo Lives Here</h1>
+                <h2 class="uk-h2">Welcome to my portfolio page.</h2>
+            </div>
+        </div>
+
+        <div class="uk-block">
+            winning
+        </div>
+
+        <div class="uk-container uk-container-center uk-text-center">
+            <h1 class="uk-heading-large">About</h1>
+            <img src="http://placehold.it/150x150" class="uk-border-circle">
+        </div>
+
+        <div class="uk-block uk-block-large uk-block-large">
+            <a href="https://github.com/nballener?tab=repositories" class="uk-icon-hover uk-icon-github uk-icon-large"></a>
+        </div>
     </body>
 </html>
